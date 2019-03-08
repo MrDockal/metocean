e22eeae3fc69e1c04c8c92493824e56ea9c1e97c
Jan Dočkal <dockal07@gmail.com>
2019-03-08 20:08:06 +1300

    Do deploy for github pages & add styled components

---
+++ src/Components/Forecast/Forecast.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { StyledForecast } from '../Styled/StyledForecast';
+
+export class Forecast extends React.Component {
+	public render() {
+		return (
+			<StyledForecast>That is forecast</StyledForecast>
+		)
+	}
+}
